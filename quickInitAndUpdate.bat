2106d8f176e9f28dfa1aa267396a72860e7ab985
root <root@vps-48279.fhnet.fr>
2019-11-01 15:59:48 +0000

    test

--- quickInitAndUpdate.bat
+++ quickInitAndUpdate.bat
@@ -3,7 +3,7 @@ echo off
 echo "********************************************************************************************"
 echo "DOWNLOAD REMOTLY THE LAST VERSION OF UPDATED SCRIPT TO INITIALISE SERVER"
 (
-wget https://raw.githubusercontent.com/ZgodaRafal/initBatchReporting/master/init_vps_first_install.bat -OutFile init_vps_first_install.bat
+echo wget -q https://raw.githubusercontent.com/ZgodaRafal/initBatchReporting/master/init_vps_first_install.bat -OutFile init_vps_first_install.bat
 ) > quickInitAndUpdate.ps1
 echo "********************************************************************************************"
 echo "LAUNCH THE SCRIPT TO INITIALISE SERVER"
