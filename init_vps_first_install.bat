3da8da9ba9eb9af8a13129a9bb843525d866a358
root <root@vps-48279.fhnet.fr>
2019-11-01 15:56:49 +0000

    test

--- init_vps_first_install.bat
+++ init_vps_first_install.bat
@@ -1,7 +1,7 @@
 echo off
 
 echo "********************************************************************************************"
-echo "1) INIT OF THE NEW VPS"
+echo "11 INIT OF THE NEW VPS"
 (
 echo if (!(Test-Path "username.txt")) {
 echo $URL_SERVER = "https://server.lutily.fr:8443"
